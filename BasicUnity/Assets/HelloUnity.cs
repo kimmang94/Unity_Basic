596e9d42604a459e88b8e47718407995897bc27e
kimhyeok <kimmang940715@naver.com>
2024-04-15 02:18:43 +0900

    feat : 생명주기 별 호출 테스트

---
+++ BasicUnity/Assets/HelloUnity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloUnity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Hello Awake");
+    }
+    void Start()
+    {
+        Debug.Log("Hello Start");
+    }
+    void OnEnable()
+    {
+        Debug.Log("Hello OnEnable");
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Hello Disable");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello Update");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Hello FiexdUpdate");
+    }
+
+    void LateUpdate()
+    {
+        Debug.Log("Hello LateUpdate");
+    }
+}
