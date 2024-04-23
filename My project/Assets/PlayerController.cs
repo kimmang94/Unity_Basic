f4d6c0a9fdbbaa2bbc5208b026d32050bc77b9b8
kimhyeok <kimmang940715@naver.com>
2024-04-23 16:15:18 +0900

    feat : 오브젝트 생성

---
+++ My project/Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // app 의 프레임 60으로 제한
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
